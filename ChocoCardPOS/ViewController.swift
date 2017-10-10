1278032c7a0c2f5f18c9da49e301eb2f4db3aafa
Eak_ <eakkachai.dev@gmail.com>
2017-10-10 17:32:31 +0700

    Initial Commit

---
+++ ChocoCardPOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChocoCardPOS
+//
+//  Created by Eak_ on 10/10/2560 BE.
+//  Copyright © 2560 Eak_. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
